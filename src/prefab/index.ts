ea18757bd07856d8cb2647208e2c69ff14c6141d
songjian <1778651752@qq.com>
2021-07-29 13:55:24 +0800

    feat: 增加对url的获取和上一页路径的保存

--- src/prefab/index.ts
+++ src/prefab/index.ts
@@ -2,16 +2,43 @@
 import * as pkg from '../../package.json'
 import UAParser, { IResult } from 'ua-parser-js'
 
+interface Url {
+  href: string
+  protocol: string
+  host: string
+  hostname: string
+  port: string
+  pathname: string
+  search: string
+  hash: string
+}
+
 interface Prefab extends IResult {
   sdkVersion: string
   network: string
+  url: Url
+  referrer: string
 }
 
 const parser = new UAParser()
 const _navigator: any = navigator
+const _location: Url = window.location
+const url: Url = {
+  href: _location.href,
+  protocol: _location.protocol,
+  host: _location.host,
+  hostname: _location.hostname,
+  port: _location.port,
+  pathname: _location.pathname,
+  search: _location.search,
+  hash: _location.hash
+}
+
 const prefab = parser.getResult() as Prefab
 
 prefab['sdkVersion'] = pkg.version
-prefab['network'] = _navigator.connection ? _navigator.connection.effectiveType : ''
+prefab['network'] = _navigator.connection ? _navigator.connection.effectiveType : undefined
+prefab['referrer'] = window.document.referrer
+prefab['url'] = url
 
 export default prefab
